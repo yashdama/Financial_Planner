aa6cd655fdaea3d7e97d836bd2adbd96bf1642de
yashwanth-dama <yash.dama96@gmail.com>
2026-09-08 11:31:51 -0400

    Completed Financial Core Calculations

--- app/finance.py
+++ app/finance.py
@@ -1,3 +1,4 @@
+import math
 
 def calculate_net_income(hourly_rate: int | float | None, 
                          monthly_salary: int | float | None, total_hours_worked:int | float | None, 
@@ -34,3 +35,67 @@ def calculate_net_income(hourly_rate: int | float | None,
     if total_deductions > gross_income:
         raise ValueError('Deductions cannot be greater than gross income')
     return float(gross_income - total_deductions)
+
+def calculate_monthly_surplus(net_income: float, recurring_expenses: list[int | float], 
+                              variable_expenses: list[int | float]) -> float:
+    if net_income < 0:
+        raise ValueError('Net Income cannot be negative')
+
+    total_recurring_expenses = 0
+    total_variable_expenses = 0
+
+    for expense in recurring_expenses:
+        if expense < 0:
+            raise ValueError('Recurring expenses cannot be negative')
+        total_recurring_expenses += expense
+    
+    for expense in variable_expenses:
+        if expense < 0:
+            raise ValueError('Variable expenses cannot be negative')
+        total_variable_expenses += expense
+
+    return float(net_income - total_recurring_expenses - total_variable_expenses)
+
+def calculate_savings_plan(current_savings: float, goal_amount: float, target_months : int, monthly_surplus: float) -> dict:
+
+    if goal_amount <= 0:
+        raise ValueError('Goal Amount must be greater than zero')
+
+    if current_savings < 0:
+        raise ValueError('Current saving cannot be negative, if no savings zero is accepted')
+
+    if target_months <= 0:
+        raise ValueError('Target months must be greater than zero')
+
+    remaining_amount = max(0, goal_amount - current_savings)
+
+    if remaining_amount == 0:
+        return {'remaining_amount': remaining_amount, 
+                'required_monthly_savings': 0.0,
+                'estimated_months': 0,
+                'achievable': True,
+                'monthly_shortfall': 0.0,
+                'monthly_cushion': 0.0
+                }
+
+    required_monthly_savings = remaining_amount / target_months
+
+    if monthly_surplus <= 0:
+        estimated_months = None
+    else:
+        estimated_months = math.ceil(remaining_amount / monthly_surplus)
+
+    achievable = monthly_surplus >= required_monthly_savings
+
+    monthly_shortfall = max(0, required_monthly_savings - monthly_surplus)
+
+    monthly_cushion = max(0, monthly_surplus - required_monthly_savings)
+
+    return {'remaining_amount': remaining_amount, 
+            'required_monthly_savings': required_monthly_savings,
+            'estimated_months': estimated_months,
+            'achievable': achievable,
+            'monthly_shortfall': monthly_shortfall,
+            'monthly_cushion': monthly_cushion
+            }
+
--- tests/test_finance.py
+++ tests/test_finance.py
@@ -1,6 +1,6 @@
 
-import pytest
-from app.finance import calculate_net_income
+import pytest, math
+from app.finance import calculate_net_income, calculate_monthly_surplus, calculate_savings_plan
 
 def test_calculate_net_income_hourly():
     assert calculate_net_income(hourly_rate=50, monthly_salary=None, total_hours_worked=160, deductions=[1000]) == 7000.0
@@ -78,3 +78,72 @@ def test_calculate_net_income_zero_hours_worked():
         ):
             calculate_net_income(hourly_rate=50, monthly_salary=None, total_hours_worked=0, deductions=[1000])
 
+def test_calculate_monthly_surplus_negative_net_income():
+    with pytest.raises(
+        ValueError,
+        match='Net Income cannot be negative'
+    ):
+        calculate_monthly_surplus(net_income=-1000, recurring_expenses=[], variable_expenses=[])
+
+def test_calculate_monthly_surplus_negative_recurring_expenses():
+    with pytest.raises(
+        ValueError,
+        match='Recurring expenses cannot be negative'
+    ):
+        calculate_monthly_surplus(net_income=1000, recurring_expenses=[-500, 100], variable_expenses=[200, 300])
+
+def test_calculate_monthly_surplus_negative_variable_expenses():
+    with pytest.raises(
+        ValueError,
+        match='Variable expenses cannot be negative'
+    ):
+        calculate_monthly_surplus(net_income=1000, recurring_expenses=[500, 100], variable_expenses=[-200, 300])
+
+def test_calculate_monthly_surplus_empty_lists():
+    assert calculate_monthly_surplus(net_income=1000, recurring_expenses=[], variable_expenses=[]) == 1000.0
+
+def test_calculate_montly_surplus_zero():
+    assert calculate_monthly_surplus(net_income=3000, recurring_expenses=[1000, 500], variable_expenses=[500, 1000]) == 0.0
+
+def test_calculate_monthy_surplus_negative_surplus():
+    assert calculate_monthly_surplus(net_income=1000, recurring_expenses=[500, 100], variable_expenses=[200, 300]) == -100.0
+
+def test_calculate_monthly_surplus_positive_surplus():
+    assert calculate_monthly_surplus(net_income=6000, recurring_expenses=[1000, 500], variable_expenses=[1000, 500]) == 3000.0
+
+def test_calculate_savings_plan_expenses():
+    assert calculate_savings_plan(current_savings=1000, goal_amount=5000, target_months=10, monthly_surplus=500) == {
+        'remaining_amount': 4000.0,
+        'required_monthly_savings': 400.0,
+        'estimated_months': 8,
+        'achievable': True,
+        'monthly_shortfall': 0.0,
+        'monthly_cushion': 100
+    }
+
+def test_calculate_savings_plan_estimated_months_None():
+    assert calculate_savings_plan(current_savings=10000, goal_amount=100000, target_months=36, monthly_surplus=0) == {
+        'remaining_amount': 90000.0,
+        'required_monthly_savings': 2500.0,
+        'estimated_months': None,
+        'achievable': False,
+        'monthly_shortfall': 2500.0,
+        'monthly_cushion': 0
+    }
+
+def test_calculate_savings_plan_estimated_months_None_shortfall_3000():
+    assert calculate_savings_plan(current_savings=10000, goal_amount=100000, target_months=36, monthly_surplus=-500) == {
+        'remaining_amount': 90000.0,
+        'required_monthly_savings': 2500.0,
+        'estimated_months': None,
+        'achievable': False,
+        'monthly_shortfall': 3000.0,
+        'monthly_cushion': 0
+    }
+
+def test_calculate_savings_plan_estimated_months_valueerror():
+    with pytest.raises(
+        ValueError,
+        match='Target months must be greater than zero'
+    ):
+        calculate_savings_plan(current_savings=100000, goal_amount=100000, target_months=0, monthly_surplus=2000)
